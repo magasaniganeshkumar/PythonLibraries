5b9d9bc6dd994d6d1564d291ea05e0c80d4e9e0a
magasaniganeshkumar <83356306+magasaniganeshkumar@users.noreply.github.com>
2021-08-05 12:54:14 +0530

    added Python program to reverse an array

---
+++ Numpy/reverse_array.py
@@ -0,0 +1,8 @@
+# Python program to reverse an array 
+import numpy as np
+array1 = np.arange(12, 38)
+print("original array is :")
+print(array1)
+print("Reverse array is :")
+reverse_array = array1[::-1]
+print(reverse_array)
