37d5efe1176e42521f256b98ffd0f205947d72b6
magasaniganeshkumar <83356306+magasaniganeshkumar@users.noreply.github.com>
2021-08-05 09:58:56 +0530

    converted a list of numeric value into a one-dimensional NumPy array

---
+++ Numpy/one_dim_numpy_array.py
@@ -0,0 +1,11 @@
+# Write a Python program to convert a list of numeric value into a one-dimensional NumPy array
+
+# importing the numpy library and change the numpy as a np
+import numpy as np
+# define one list of elements
+list1 = [12.23, 13.32, 100, 36.32]
+# printing the list of elements
+print("Original List:", list1)
+# converting list1 into one-dimensional NumPy array
+array1 = np.array(list1)
+print("One-dimensional NumPy array: ", array1)
