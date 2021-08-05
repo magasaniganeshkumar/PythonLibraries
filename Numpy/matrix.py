15b1358926719801fd76de50408642e867785ab5
magasaniganeshkumar <83356306+magasaniganeshkumar@users.noreply.github.com>
2021-08-05 12:31:36 +0530

    Created  3x3 matrix with values range from 2 to 10

---
+++ Numpy/matrix.py
@@ -0,0 +1,7 @@
+# Write a Python program to Create a 3x3 matrix with values ranging from 2 to 10. 
+
+# importing the numpy library and change the numpy as a np
+import numpy as np
+matrix =  np.arange(2, 11).reshape(3,3)
+# printing 3x3 matrix 
+print(matrix)
