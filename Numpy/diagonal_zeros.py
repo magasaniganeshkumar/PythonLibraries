b1af796ec10431797f9a182caef0e11977dc5ee9
magasaniganeshkumar <83356306+magasaniganeshkumar@users.noreply.github.com>
2021-08-05 19:56:48 +0530

    added Python program to 3-D array with ones on a diagonal and zeros elsewhere

---
+++ Numpy/diagonal_zeros.py
@@ -0,0 +1,7 @@
+# Python program to create a 3-D array with ones on a diagonal and zeros elsewhere
+# importing the numpy library
+import numpy as np
+
+array1 = np.eye(3)
+print('3-D array with ones on a diagonal and zeros elsewhere:')
+print(array1)
