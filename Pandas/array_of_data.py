ff1928b6cc0341c9392dbac750bba9a75ac0183d
magasaniganeshkumar <83356306+magasaniganeshkumar@users.noreply.github.com>
2021-08-08 17:12:05 +0530

    Added a python programme to containing an array of data using Pandas module

---
+++ Pandas/array_of_data.py
@@ -0,0 +1,7 @@
+"""
+Write a Python program to create and display a one-dimensional array-like object containing an array of data using Pandas module
+"""
+# importing  pandas library
+import pandas as pd
+data = pd.Series([2, 4, 6, 8, 10])
+print(data)
